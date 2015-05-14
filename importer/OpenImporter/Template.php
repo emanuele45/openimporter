45f2fc6f306841d665c30b5b7ab55cc1361d8c0a
emanuele <emanuele45@gmail.com>
2015-05-14 21:26:14 +0200

    Moved all the remaining templates to Twig
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/OpenImporter/Template.php
+++ importer/OpenImporter/Template.php
@@ -99,7 +99,11 @@ class Template
 		}
 
 		if ($this->response->is_page && $this->response->template_error)
-			$this->renderErrors();
+		{
+			$replaces['template'] = array('step' => $this->config->progress->current_step);
+			$render = $this->twig->loadTemplate('renderErrors.html');
+			echo $render->render($replaces);
+		}
 
 		$templates = $this->response->getTemplates();
 		foreach ($templates as $template)
@@ -121,95 +125,5 @@ class Template
 				$render = $this->twig->loadTemplate('footer.html');
 				echo $render->render($replaces);
 			}
-	}
-
-	public function step0(Form $form)
-	{
-		echo '
-			<h2>', $this->lng->get('before_continue'), '</h2>
-			<div class="content">
-				<p>', sprintf($this->lng->get('before_details'), $this->response->source_name, $this->response->destination_name), '</p>
-			</div>';
-		$form->title = $this->lng->get('where');
-		$form->description = $this->lng->get('locate_destination');
-		$form->submit = array(
-			'name' => 'submit_button',
-			'value' => $this->lng->get('continue'),
-		);
-		$this->renderForm($form);
-
-		echo '
-			<div class="content">
-				<h3>', $this->lng->get('not_this'),'</h3>
-				<p>', $this->lng->get(array('pick_different', $this->response->scripturl . '?action=reset')), '</p>
-			</div>';
-	}
-
-	public function renderForm(Form $form)
-	{
-		echo '
-			<h2>', $form->title, '</h2>
-			<div class="content">
-				<form action="', $form->action_url, '" method="post">
-					<p>', $form->description, '</p>
-					<dl>';
-
-		foreach ($form->options as $option)
-		{
-			if (empty($option))
-			{
-				echo '
-					</dl>
-					<div id="toggle_button">', $this->lng->get('advanced_options'), ' <span id="arrow_down" class="arrow">&#9660</span><span id="arrow_up" class="arrow">&#9650</span></div>
-					<dl id="advanced_options">';
-				continue;
-			}
-
-			switch ($option['type'])
-			{
-				case 'text':
-					echo '
-						<dt><label for="', $option['id'], '">', $option['label'], ':</label></dt>
-						<dd>
-							<input type="text" name="', $option['id'], '" id="', $option['id'], '" value="', $option['value'], '" ', !empty($option['validate']) ? 'onblur="validateField(\'' . $option['id'] . '\')"' : '', ' class="text" />
-							<div id="validate_', $option['id'], '" class="validate">', $option['correct'], '</div>
-						</dd>';
-					break;
-				case 'checkbox':
-					echo '
-						<dt></dt>
-						<dd>
-							<label for="', $option['id'], '">', $option['label'], ':
-								<input type="checkbox" name="', $option['id'], '" id="', $option['id'], '" value="', $option['value'], '" ', $option['attributes'], '/>
-							</label>
-						</dd>';
-					break;
-				case 'password':
-					echo '
-						<dt><label for="', $option['id'], '">', $option['label'], ':</label></dt>
-						<dd>
-							<input type="password" name="', $option['id'], '" id="', $option['id'], '" class="text" />
-							<div class="passwdcheck">', $option['correct'], '</div>
-						</dd>';
-					break;
-				case 'steps':
-					echo '
-						<dt><label for="', $option['id'], '">', $option['label'], ':</label></dt>
-						<dd>';
-						foreach ($option['value'] as $key => $step)
-							echo '
-							<label><input type="checkbox" name="do_steps[', $key, ']" id="do_steps[', $key, ']" value="', $step['count'], '"', $step['mandatory'] ? ' readonly="readonly" ' : ' ', $step['checked'], ' /> ', $step['label'], '</label><br />';
-
-					echo '
-						</dd>';
-					break;
-			}
-		}
-
-		echo '
-					</dl>
-					<div class="button"><input id="submit_button" name="', $form->submit['name'], '" type="submit" value="', $form->submit['value'],'" class="submit" /></div>
-				</form>
-			</div>';
 	}
 }
